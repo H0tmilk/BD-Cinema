8030f0b8fcad880f0dd52620617d481f7e16cea6
H0tmilk <chollet.kilian@gmail.com>
2017-09-16 16:13:58 +0200

    Légères corrections

--- data-struct.h
+++ data-struct.h
@@ -5,15 +5,15 @@
 typedef enum Genre {ACTION, HORREUR, COMEDIE, DOCUMENTAIRE, POLICIER, DRAME, ANIMATION,SCIENCEFICTION} Genre;
 
 // On se limite aux nations que nous utiliserons
-typedef enum Nation{FRANCEn USA, CANADA} Nation;
+typedef enum Nation{FRANCE, USA, UK, CANADA} Nation;
 
-typedef struct date{
+typedef struct date {
     int annee;
     char mois[10];
     int jour;
 } date;
 
-typedef struct personne{
+typedef struct personne {
     unsigned int id;
     char* nom;
     char* prenom;
